335c7d8797cc15080b4f4ab44f8680462b642101
Ameer Bakiev <roariffic@icloud.com>
2024-09-18 17:29:43 +0500

    change user group chatsto many to many and created groups table

--- bot.py
+++ bot.py
@@ -14,9 +14,6 @@ async def handleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text: str = update.message.text
     user = update.message.from_user
     username = user['username']
-#     print(f'''{update.message.chat['id']}
-# {update.message.chat['title']}
-# {update.message.chat['type']}''')
     link = findLink(text)
     if link is not None:
         if 'tiktok.com' in link:
@@ -34,11 +31,13 @@ async def handleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 deleteVideo(fileName)
             else:
                 await update.message.reply_text("Failed to download video.")
+
     if username != 'amialmighty':
         chance = randint(0, 100)
         # print(username + ': ' + str(chance))
         if chance == 69:
             await update.message.reply_text(choice(responds))
+
     if text == '/create chat':
         chatId = update.message.chat['id']
         title = update.message.chat['title']
@@ -50,34 +49,77 @@ async def handleMessage(update: Update, context: ContextTypes.DEFAULT_TYPE):
         else:
             await update.message.reply_text('Что-то пошло не так')
     
-    if text == '/create me':
+    elif text == '/create me':
         chatId = update.message.chat['id']
         userId = update.message.from_user['id']
         firstName = update.message.from_user['first_name']
         username = update.message.from_user['username']
         db = DataBase()
-        status = db.createUser(chatId, userId, firstName, username)
-        if status == True:
+        status1 = db.createUser(userId, firstName, username)
+        status2 = db.addGroupChatToUser(userId, chatId)
+        if status1 and status2:
             await update.message.reply_text('Пользователь был успешно добавлен')
         else:
             await update.message.reply_text('Что-то пошло не так')
 
-    if text == '/update me':
+    elif text == '/update me':
         userId = update.message.from_user['id']
         firstName = update.message.from_user['first_name']
         username = update.message.from_user['username']
-        db = DataBase()
-        status = db.updateUser(userId, firstName, username)
+        status = DataBase().updateUser(userId, firstName, username)
         if status == True:
             await update.message.reply_text('Пользователь был успешно изменен')
         else:
             await update.message.reply_text('Что-то пошло не так')
+    elif text.startswith('/create group '):
+        chatId = update.message.chat['id']
+        message = DataBase().createGroup(text, chatId)
+        await update.message.reply_text(message)
+
+    elif text.startswith('/delete group name:'):
+        chatId = update.message.chat['id']
+        status = DataBase().deleteGroup(text, chatId)
+        if status:
+            await update.message.reply_text('Group was succesfully delited')
+        else:
+            await update.message.reply_text('Something went wrong')
+
+    elif text.startswith('/add to group '):
+        chatId = update.message.chat['id']
+        message = DataBase().addUsersToGroup(text, chatId)
+        await update.message.reply_text(message)
+
+    elif text.startswith('/delete users group '):
+        chatId = update.message.chat['id']
+        message = DataBase().deleteUsersFromGroup(text, chatId)
+        await update.message.reply_text(message)
         
     if '@TikTokDownloaderRusBot' in text:
         await update.message.reply_text(choice(respondsOld))
+    print(text)
+    if text == '/get_commands@TikTokDownloaderRusBot':
+        comandList = """/create chat - Add current chat to bot DB
+/create me - Add current user to bot DB
+/update me - Update user info in bot DB
+/create group name:{name} users:{username},{username} - Add group to chat
+/add to group name:{name} users:{username},{username} - Add users to group
+/delete group name:{name} - Delete group from chat
+/delete users group name:{name} users:{username},{username}- delete users from group"""
+        await update.message.reply_text(comandList)
+
     if '@all' in text:
         usernames = DataBase().getAllUsernames(update.message.chat['id'])
         await update.message.reply_text(usernames)
+    elif '@' in text:
+        groups = DataBase().getAllGroups(update.message.chat['id'])
+        for group in groups:
+            if '@'+group['name'] in text:
+                groupName = group['name'].replace('@', '')
+                chatId = update.message.chat['id']
+                currentGroup = DataBase().getGroupByGroupChatIdAndName(chatId, groupName)
+                editedText = text.replace('@'+groupName, '')
+                respondText = DataBase().getUsernamesByGroup(currentGroup['id'])+'\n'+editedText
+                await update.message.reply_text(respondText)
 def main():
     app = Application.builder().token(TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT, handleMessage))
--- database.py
+++ database.py
@@ -1,109 +1,325 @@
 import sqlite3
+import re
 
 class DataBase:
     def __init__(self):
         self.conn = sqlite3.connect('main.db')
         self.curr = self.conn.cursor()
+        # create group chat table
         self.curr.execute("""
             create table if not exists group_chats(
-                id integer primary key,
-                chat_id integer,
+                id integer,
                 title varchar(255),
                 type varchar(30)
             )
         """)
+        # create users table
         self.curr.execute("""
             create table if not exists users(
-                id integer primary key,
-                group_chat_id integer,
-                user_id integer,
+                id integer,
                 first_name varchar(255),
-                username varchar(255),
+                username varchar(255)
+            )
+        """)
+        # create many to many between users and group_chats
+        self.curr.execute("""
+            create table if not exists user_group_chats(
+                user_id integer,
+                group_chat_id integer,
+                foreign key(user_id) references users(id),
+                foreign key(group_chat_id) references group_chats(id)
+            )
+        """)
+        # create groups table
+        self.curr.execute("""
+            create table if not exists groups(
+                id integer primary key,
+                name varchar(255),
+                group_chat_id integer,
                 foreign key(group_chat_id) references group_chat(id)
             )
         """)
-    def createGroupChat(self, chat_id, title, type):
-        if self.getGroupChatId(chat_id) == None:
-            self.curr.execute('insert into group_chats(chat_id, title, type) values(?,?,?)', (chat_id, title, type))
-            self.conn.commit()
-            print('group chat has been created')
-            return True
-        return False
-    
-    def getGroupChatId(self, chat_id):
-        result = self.curr.execute('select (id) from group_chats where chat_id = ? limit 1', (chat_id,))
-        row = result.fetchone()
-        if row != None:
-            return row[0]
-        return None
-    
-    def getUserId(self, user_id):
-        result = self.curr.execute('select (id) from users where user_id = ? limit 1', (user_id,))
-        row = result.fetchone()
-        if row != None:
-            return row[0]
-        return None
-    
-    def getAllUserIds(self, user_id):
-        result = self.curr.execute('select (id) from users where user_id = ?', (user_id,))
-        rows = result.fetchall()
-        if rows != None:
-            ids = []
-            for id in rows:
-                ids.append(id[0])
-            return ids
-        return None
-    
-    def getAllUsernames(self, chat_id):
-        groupChatId = self.getGroupChatId(chat_id)
-        result = self.curr.execute('select (username) from users where group_chat_id = ?', (groupChatId,))
-        rows = result.fetchall()
-        usernames = []
-        for name in rows:
-            usernames.append('@'+name[0])
-        usernamesString = ', '.join(usernames)
-        if rows != None:
-            return usernamesString
-        return 'Что-то пошло не так'
-        
-    
-    def isUserAlreadyInGroupChat(self, user_id, groupChatId):
-        result = self.curr.execute('select (id) from users where user_id = ? and group_chat_id = ? limit 1', (user_id, groupChatId))
-        row = result.fetchone()
-        if row != None:
-            return True
-        return False
+        # create many to many between users and groups
+        self.curr.execute("""
+            create table if not exists user_groups(
+                user_id integer,
+                group_id integer,
+                foreign key(user_id) references users(id),
+                foreign key(group_id) references groups(id)
+            )
+        """)
 
-    def createUser(self, chat_id, user_id, first_name, username):
-        group_chat_id = self.getGroupChatId(chat_id)
-        if not self.isUserAlreadyInGroupChat(user_id, group_chat_id):
-            self.curr.execute('insert into users(group_chat_id, user_id, first_name, username) values(?,?,?,?)', (group_chat_id, user_id, first_name, username))
+
+    def getUser(self, id):
+        result = self.curr.execute('select * from users where id = ? limit 1', (id,))
+        row = result.fetchone()
+        if row != None:
+            return {
+                'id': row[0],
+                'first_name': row[1],
+                'username': row[2]
+            }
+        return None
+    
+    def getUserbyUsername(self, username):
+        result = self.curr.execute('select * from users where username = ? limit 1', (username,))
+        row = result.fetchone()
+        if row != None:
+            return {
+                'id': row[0],
+                'first_name': row[1],
+                'username': row[2]
+            }
+        return None
+    
+    def createUser(self, id, first_name, username):
+        user = self.getUser(id)
+        if user == None:
+            self.curr.execute('insert into users(id, first_name, username) values(?,?,?)', (id, first_name, username))
             self.conn.commit()
             print('user has been created')
             return True
+        print('user was already created')
         return False
     
-    def updateUser(self, user_id, first_name, username):
-        ids = self.getAllUserIds(user_id)
-        for id in ids:
-            self.curr.execute('update users set first_name = ?, username = ? where id = ?', (first_name, username, id))
-            self.conn.commit()
-        print('all user info has been updated')
-        return True
-
+    def addGroupChatToUser(self, userId, chatId):
+        user = self.getUser(userId)
+        if user == None:
+            print('user was not found')
+            return False
         
+        if self.isUserAlreadyInGroupChat(userId, chatId):
+            print('user already linked to that chat')
+            return False
+        self.curr.execute('insert into user_group_chats(user_id, group_chat_id) values(?,?)', (userId, chatId))
+        self.conn.commit()
+        print('user has been added to group chat')
+        return True
+    
+    def createGroupChat(self, chatId, title, type):
+        if self.getGroupChat(chatId) == None:
+            self.curr.execute('insert into group_chats(id, title, type) values(?,?,?)', (chatId, title, type))
+            self.conn.commit()
+            print('group chat has been created')
+            return True
+        print('group chat is already created')        
+        return False
+    
+    def getGroupChat(self, chatId):
+        result = self.curr.execute('select * from group_chats where id = ? limit 1', (chatId,))
+        row = result.fetchone()
+        if row != None:
+            return {
+                'id':row[0],
+                'title':row[1],
+                'type':row[2]
+            }
+        return None
+    
+    def getAllUsernames(self, chat_id):
+        result = self.curr.execute(
+        """
+        SELECT users.username
+        FROM users
+        JOIN user_group_chats ON user_group_chats.user_id = users.id
+        WHERE user_group_chats.group_chat_id = ?
+        """,
+        (chat_id,)
+    )
+        rows = result.fetchall()
+        print(rows)
+        usernames = ['@' + name[0] for name in rows]
+        usernamesString = ', '.join(usernames)
+        return usernamesString if rows else 'Something went wrong'
+    
+    def isUserAlreadyInGroupChat(self, userId, groupChatId):
+        result = self.curr.execute('select * from user_group_chats where user_id = ? and group_chat_id = ? limit 1', (userId, groupChatId))
+        row = result.fetchone()
+        if row != None:
+            return True
+        return False
+    
+    def isUserAlreadyInGroup(self, userId, groupId):
+        result = self.curr.execute('select * from user_groups where user_id = ? and group_id = ? limit 1', (userId, groupId))
+        row = result.fetchone()
+        if row != None:
+            return True
+        return False
+    
+    def updateUser(self, userId, firstName, username):
+        user = self.getUser(userId)
+        if user == None:
+            print('User with that id does not exists')
+            return False
+        if user['first_name'] == firstName and user['username'] == username:
+            print('There is nothing new about you')
+            return False
+        self.curr.execute('update users set first_name = ?, username = ? where id = ?', (firstName, username, userId))
+        self.conn.commit()
+        print('User info has been updated')
+        return True
+    
+    def getGroupByGroupChatIdAndName(self, groupChatId, name):
+        result = self.curr.execute('select * from groups where group_chat_id = ? and name = ? limit 1', (groupChatId, name))
+        row = result.fetchone()
+        if row != None:
+            return {
+                'id': row[0],
+                'name': row[1],
+                'groupChatId':row[2]
+            }
+        return None
+    
+    def parseCommand(self, command):
+            result = {
+                'success': False,
+                'groupName': None,
+                'users': []
+            }
+
+            pattern = r"name:(?P<name>\w+)(?: users:(?P<users>\w+(?:,\s*\w+)*))?"
+            match = re.match(pattern, command)
+
+            if match:
+                result['success'] = True
+                result['groupName'] = match.group('name')
+                users_match = match.group('users')
+                if users_match:
+                    result['users'] = re.findall(r"\w+", users_match)
+            return result
+    
+    def createGroup(self, command, groupChatId):
+        command = command.replace('@', '')
+        command = command.replace('/create group ', '')
+        message = ''
+        result = self.parseCommand(command)
+        print(result)
+        group = self.getGroupByGroupChatIdAndName(groupChatId, result['groupName'])
+        if group != None:
+            message = message+'Group @'+result['groupName']+' is already exists'
+        else:
+            self.curr.execute('insert into groups(name, group_chat_id) values(?,?)', (result['groupName'], groupChatId))
+            self.conn.commit()
+            message = message+'Group @'+result['groupName']+' has been created'
+            group = self.getGroupByGroupChatIdAndName(groupChatId, result['groupName'])
+        query = self.curr.execute('select id from groups where name = ? and group_chat_id = ? limit 1', (result['groupName'], groupChatId))
+        groupId = query.fetchone()[0]
+        for username in result['users']:
+            user = self.getUserbyUsername(username)
+            if user == None:
+                message = message +'\nUser @' + username + ' was not found'
+            else:
+                self.curr.execute('insert into user_groups(user_id, group_id) values(?,?)', (user['id'], groupId))
+                self.conn.commit()
+                message = message+'\nUser @' + username + ' has been added'
+        return message
+    
+    def deleteGroup(self, command, groupChatId):
+        command = command.replace('@', '')
+        groupName = command.replace('/delete group name:', '')
+        groupName = groupName.replace(' ', '')
+        group = self.getGroupByGroupChatIdAndName(groupChatId, groupName)
+        if group is not None:
+            self.curr.execute('delete from user_groups where group_id = ?', (group['id'],))
+            self.conn.commit()
+            self.curr.execute('delete from groups where id = ?', (group['id'],))
+            self.conn.commit()
+            print('group has been deleted')
+            return True
+        else:
+            print('group was not found')
+            return False
+
+    def addUsersToGroup(self, command, groupChatId):
+        command = command.replace('@', '')
+        command = command.replace('/add to group ', '')
+        print(command)
+        result = self.parseCommand(command)
+        group = self.getGroupByGroupChatIdAndName(groupChatId, result['groupName'])
+        if group is not None:
+            message = ''
+            for username in result['users']:
+                user = self.getUserbyUsername(username)
+                if user == None:
+                    message = message +'\nUser @' + username + ' was not found'
+                else:
+                    if self.isUserAlreadyInGroup(user['id'], group['id']):
+                        message = message + '\nUser @' + username + ' is already in group'
+                    else:
+                        self.curr.execute('insert into user_groups(user_id, group_id) values (?, ?)', (user['id'], group['id']))
+                        self.conn.commit()
+                        message = message+'\nUser @' + username + ' has been added'
+            return message
+        return 'Group @'+result['groupName']+'was not found'
+    
+    def deleteUsersFromGroup(self, command, groupChatId):
+        command = command.replace('@', '')
+        command = command.replace('/delete users group ', '')
+        result = self.parseCommand(command)
+        group = self.getGroupByGroupChatIdAndName(groupChatId, result['groupName'])
+        if group is not None:
+            message = ''
+            for username in result['users']:
+                user = self.getUserbyUsername(username)
+                if user == None:
+                    message = message +'\nUser @' + username + ' was not found'
+                else:
+                    self.curr.execute('delete from user_groups where user_id = ? and group_id = ?', (user['id'], group['id']))
+                    self.conn.commit()
+                    message = message+'\nUser @' + username + ' has been deleted'
+            return message
+        return 'Group @'+result['groupName']+'was not found'
+    
+    def getAllGroups(self, groupChatId):
+        result = self.curr.execute('select * from groups where group_chat_id = ?', (groupChatId,))
+        rows = result.fetchall()
+        return [{'id':row[0], 'name':row[1], 'group_chat_id':row[2]} for row in rows] if rows else 'Something went wrong'
+    
+    def getUsernamesByGroup(self, groupId):
+        result = self.curr.execute(
+            """
+            SELECT users.username
+            FROM user_groups
+            JOIN users ON user_groups.user_id = users.id
+            WHERE user_groups.group_id = ?
+            """,
+            (groupId,)
+        )
+        rows = result.fetchall()
+        usernames = ['@' + name[0] for name in rows]
+        usernamesString = ', '.join(usernames)
+        return usernamesString if rows else 'Something went wrong'
+    
 
 
+    
 
 
 def main():
+    CHAT_ID = -4083204401
     db = DataBase()
-    # db.createGroupChat(-4083204401, 'friends-crossover', 'group')
-    # db.createUser(-602765732, 1646306858, 'Gosha', 'songoshan')
-    db.curr.execute('update users set first_name = "rusio" where id = 1')
-    db.conn.commit()
-
-    # print(db.getGroupChatId(-4083204401))
+    db.createGroupChat(1, 'fake', 'group')
+    db.createUser(123, 'user', 'rus')
+    db.addGroupChatToUser(123, 1)
+    db.createUser(124, 'sada', 'gosha')
+    db.addGroupChatToUser(124, 1)
+    # db.createUser(1646306856, 'qweqwe', 'test')
+    # db.addGroupChatToUser(1646306856, CHAT_ID)
+    creatGroupString = '/create group name:zek users:@Rus_Shiga,@songoshan '
+    addUsersToGroupString = '/group name:gymik users:@rus,@gosha,  @petya'
+    deleteGroupString = '/delete group name:zek'
+    deleteUsersGroupString = '/delete users group name:gymik users:@rus,@gosha,  @petya'
+    addUsersGroupString = '/add to group name:gymik100 users:@gosha'
+    queryString = '/create group name:gymik100 users:@rus,  @petya'
+    # print(db.deleteGroup(deleteGroupString, CHAT_ID))
+    # print(db.createGroup(queryString, CHAT_ID))
+    # print(db.addUsersToGroup(addUsersGroupString, CHAT_ID))
+    # print(parse_command(queryString))
+    # print(db.getUsernamesByGroup(1))
+    # db.deleteGroup(deleteGroupString, CHAT_ID)
+    # db.createGroup(creatGroupString, CHAT_ID)
+    # print(db.getUsernamesByGroup(1))
+    print(db.getAllUsernames(1))
 
 if __name__ == '__main__':
     main()
